cb6f45b591ff5f9053539b483846cdb1662dc426
kaich <chengkai1853@163.com>
2016-05-05 16:00:47 +0800

    fix progress bug and change structure

--- Example/ASIHTTPDownloadManager/Pods/Target Support Files/Pods-CKDownloadManager_Example/Pods-CKDownloadManager_Example-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_CKDownloadManager_ExampleVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_CKDownloadManager_ExampleVersionString[];
-
